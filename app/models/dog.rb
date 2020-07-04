48ec47e05e9777cb5279eca332c107814b4e1393
Chris Kay <ckay84@protonmail.com>
2020-07-04 12:04:29 -0400

    Added pry and pry-doc gems to Gemfile. Adjusted iterator on Dog.import method in Dog model.

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,7 +2,7 @@ class Dog < ActiveRecord::Base
   require 'csv'
   
   def self.import(file)
-    CSV.each(file.path, headers: true) do |row|
+    CSV.foreach(file.path, headers: true) do |row|
       Dog.create! row.to_hash
     end
   end
